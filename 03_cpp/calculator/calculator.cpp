ab5741d4595580e32c6aceb42c4794b9385a648f
Nikolai <kolyan.zherbin@gmail.com>
2022-11-10 22:03:17 +0300

    add calculator.cpp

---
+++ 03_cpp/calculator/calculator.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+double a, b, o;
+char c;
+while (true)
+	{
+	cin >> a;
+	cin >> c;
+	cin >> b;
+	    if (c == '+'){
+        cout << a << "+" <<  b << "=" << a + b;
+	    }
+	    if (c == '-'){
+      cout << a << "-" <<  b << "=" <<  a - b;
+	    }
+	    if (c == '*'){
+      cout << a << "*" <<  b << "=" <<  a * b;
+	    }
+	    if (c == '/'){
+	        if (b == 0){
+      cout << "undefined";
+	        }
+	        else {
+      cout << a << "/" <<  b << "=" <<  a / b;    
+      }
+	  }
+	    o = 1;
+	    if (c == '^'){
+	        for (double i = b; i > 0; i--) {
+	            o = o * a;
+	        }  
+      cout << a << "^" <<  b << "=" << o;
+    }
+}
+	
+}
