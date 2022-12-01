41b91bf5d01f7049c751eb5c2e612922215d12f3
Nikolai <kolyan.zherbin@gmail.com>
2022-12-01 13:14:51 +0300

    add sort-cmp.cpp

--- 04_search_sort/sort_cmp/sort-cmp.cpp
+++ 04_search_sort/sort_cmp/sort-cmp.cpp
@@ -1,3 +1,4 @@
+
 #include <chrono>
 #include <cmath>
 #include <cstdio>
@@ -12,32 +13,31 @@ int partition(int arr[], int lo, int hi);
 int *swap(int arr[], int what, int with);
 
 int main() {
-  long long int help[] = {
-   10,
-   100,
-   1000,
-   1000000, // dead point
-   1000000000,
-   1000000000000
-  };
+  long long int help[] = {10,         100,          1000,
+                          1000000, // dead point
+                          1000000000, 1000000000000};
 
   for (int i = 0; i != 5; ++i) {
+
     auto n = help[i];
+
     int arr1[n], arr2[n];
+
     fill_array_random(arr1, n, 0, 10000);
     memcpy(arr2, arr1, n * sizeof(int));
+
     auto t1 = std::chrono::high_resolution_clock::now();
     selection_sort(arr1, n);
     auto t2 = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double, std::milli> ss_ms = t2 - t1;
+
     t1 = std::chrono::high_resolution_clock::now();
     quick_sort(arr2, 0, n - 1);
     t2 = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double, std::milli> qs_ms = t2 - t1;
-    std::printf("%.0e: %.5f (ss), %.5f (qs)\n",
-                static_cast<double>(n),
-                ss_ms.count(),
-                qs_ms.count());
+
+    std::printf("%.0e: %.5f (ss), %.5f (qs)\n", static_cast<double>(n),
+                ss_ms.count(), qs_ms.count());
   }
   return 0;
 }
@@ -46,10 +46,12 @@ void fill_array_random(int arr[], int n, int a, int b) {
   std::random_device dev;
   std::mt19937 rng(dev());
   std::uniform_int_distribution<std::mt19937::result_type> dist(a, b);
+
   for (int i = 0; i < n; ++i) {
     arr[i] = dist(rng);
   }
 }
+
 void selection_sort(int arr[], int n) {
   for (int i = 0; i < n - 1; i++) {
     int jmin = i;
@@ -94,3 +96,5 @@ int *swap(int arr[], int what, int with) {
   arr[with] = tmp;
   return arr;
 }
+
+
